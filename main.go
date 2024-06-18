1aebeeaba0bd40cb93df1502cab4fb891c848a0e
pedrodecf <pedro012014@gmail.com>
2024-06-18 11:15:17 -0300

    creating routes

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/pedrodecf/go-crud-study/src/controller/routes"
 )
 
 func main() {
@@ -11,4 +13,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup)
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
 }
