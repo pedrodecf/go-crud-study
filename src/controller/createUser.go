820544e5e75679312befe3543aa6912d70cca927
pedrodecf <pedro012014@gmail.com>
2024-06-24 10:39:08 -0300

    add logs

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,21 +1,40 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pedrodecf/go-crud-study/src/configuration/logger"
 	"github.com/pedrodecf/go-crud-study/src/configuration/validation"
 	"github.com/pedrodecf/go-crud-study/src/controller/model/request"
+	"github.com/pedrodecf/go-crud-study/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("init createUser controller",
+		zap.String("journey", "createUser"))
+
 	var userRequest request.UserRequest
-	
+
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("error trying to validate user info", err,
+			zap.String("journey", "createUser"))
+
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("user created successfully",
+		zap.String("journey", "createUser"))
+
+	c.JSON(http.StatusOK, response)
 }
